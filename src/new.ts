31cdf5540efa5e4b172b9878b477c919855cb86d
thanhgit123 <hai880345@gmail.com>
2024-03-27 10:53:23 +0700

    tesstts

--- src/new.ts
+++ src/new.ts
@@ -1,8 +1,4 @@
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const arr2 = [...arr, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 const a=2
 const b=3
-const total = a+b
+const total = a+b+2300
 console.log('11',total)
-console.log('11',arr)
-console.log('22',arr2)
